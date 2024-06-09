475a9218efd38fa07b7bc003b1d08f9b651f0441
Rishabh sati <rishabhsati6505@gmail.com>
2024-06-09 12:40:49 +0530

    solved fibonacci problem

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function fibonacci(num) {
-// your code here
+  // your code here
+  if (num === 0) return 0;
+  if (num === 1) return 1;
+
+  let prev = 0;
+  let curr = 1;
+  let next = 0;
+
+  for (let i = 2; i <= num; i++) {
+    let next = prev + curr;
+    prev = curr;
+    curr = next;
+  }
+  return prev;
 }
 
+console.log(fibonacci(1));
+console.log(fibonacci(5));
 module.exports = fibonacci;
